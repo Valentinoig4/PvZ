47db0829b6658559466ac9500c151929b76cc6ed
Valentino <fvalentino.pe@gmail.com>
2025-11-29 19:03:11 -0500

    Add Oleadas class with wave management functionality

---
+++ Oleadas.h
@@ -0,0 +1,15 @@
+#ifndef OLEADAS_H
+#define OLEADAS_H
+
+class Oleadas {
+public:
+    int numeroOleada;
+
+    Oleadas() : numeroOleada(1) {}
+
+    void siguienteOleada() {
+        numeroOleada++;
+    }
+};
+
+#endif
