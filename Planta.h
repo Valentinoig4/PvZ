d80580098874c667cc6d08806de4d43641f0b572
Sebastian <sebastian.casas.m@utec.edu.pe>
2025-11-30 17:35:27 -0500

    planta

--- Planta.h
+++ Planta.h
@@ -1,14 +1,25 @@
 #ifndef PLANTA_H
 #define PLANTA_H
 
+#include "Zombie.h"
+
+enum class tipoPlanta {
+    atacante,productor, soporte
+};
+
 class Planta {
 protected:
-    int vida{}, costo{}, nivel{};
-    float demora{};
+    int vida{}, costo{}, nivel{}, ticks{}, demora{};
 public:
+    tipoPlanta tipo;
     Planta() = default;
-    Planta(int v, float d, int c, int n) : vida(v), costo(c), nivel(n), demora(d) {};
-    virtual void accion() = 0;
+    Planta(tipoPlanta t, int v, float d, int c, int n) : vida(v), costo(c), nivel(n), demora(d), tipo(t) {}
+    bool anadirTick() {
+        ticks++;
+        if (tipo != tipoPlanta::soporte && demora > 0)
+            return ticks % demora == 0;
+        return false;
+    }
     void recibir_dano(int dano) {
         vida -= dano;
         vida < 0 ? vida = 0 : vida;
@@ -19,30 +30,37 @@ class Atacante : public Planta {
 protected:
     int ataque{};
 public:
-    Atacante() = default;
-    Atacante(int v, int a, float d, int c, int n) : Planta(v, d, c, n), ataque(a) {}
+    Atacante() { tipo = tipoPlanta::atacante; }
+    Atacante(int v, int a, int d, int c, int n) : Planta(tipoPlanta::atacante, v, d, c, n), ataque(a) {}
+    virtual void accion(Zombie& z) = 0;
 };
 
 class Productor : public Planta {
 protected:
-    int soles;
+    int soles{};
 public:
-    Productor() = default;
-    Productor(int v, int s, int c, int n, float d) : Planta(v, d, c, n), soles(s) {}
+    Productor() { tipo = tipoPlanta::productor; }
+    Productor(int v, int s, int c, int n, int d) : Planta(tipoPlanta::productor, v, d, c, n), soles(s) {}
+    virtual void accion() = 0;
 };
 
 class Soporte : public Planta {
 public:
-    Soporte() = default;
-    Soporte(int v, int c, int n, float d) : Planta(v, d, c, n) {}
+    Soporte() { tipo = tipoPlanta::soporte; };
+    Soporte(int v, int c, int n, int d) : Planta(tipoPlanta::soporte, v, d, c, n) {}
 };
 
 class LanzaGuisantes : public Atacante {
 public:
-    LanzaGuisantes() : Atacante(300, 20, 1.425, 100, 1) {}
-    void accion () override {
-        //atacar a zombie con ataque
+    LanzaGuisantes() : Atacante(300, 20, 3, 100, 1) {}
+    void accion (Zombie& z) override {
+        z.recibirDanio(ataque);
     }
+};
+
+class LanzaGuisantesDoble : public Atacante {
+public:
+    LanzaGuisantesDoble() : Atacante(200, 20, 3, 100, 1) {}
 };
 
 #endif //PLANTA_H
