b12579963c1142cf157ce7f72e153874b2a5267b
rafaelchamayo <166565882+rafaelchamayo@users.noreply.github.com>
2025-12-02 05:39:25 -0500

    plantaqueFunciona actualizado al final

--- plantaqueFunciona.h
+++ plantaqueFunciona.h
@@ -12,13 +12,14 @@ class Planta {
 public:
     int vida{}, costo{}, nivel{}, ticks{}, demora{};
     int ataque{};
+    char ID{};
 
     virtual ~Planta() = default;
 
     tipoPlanta tipo{};
     Planta() = default;
-    Planta(tipoPlanta t, int v, int d, int c, int n) : vida(v), costo(c), nivel(n), demora(d), tipo(t) {}
-    Planta(tipoPlanta t, int v, int d, int c, int n, int a) : vida(v), costo(c), nivel(n), demora(d), tipo(t), ataque(a) {}
+    Planta(tipoPlanta t, int v, int d, int c, int n, char id) : vida(v), costo(c), nivel(n), demora(d), tipo(t), ID(id) {}
+    Planta(tipoPlanta t, int v, int d, int c, int n, int a, char id) : vida(v), costo(c), nivel(n), demora(d), tipo(t), ataque(a), ID(id) {}
     bool anadirTick() {
         ticks++;
         if (tipo != tipoPlanta::soporte && demora > 0)
@@ -40,7 +41,7 @@ public:
     class Atacante : public Planta {
 public:
     Atacante() { tipo = tipoPlanta::atacante; }
-    Atacante(int v, int a, int d, int c, int n) : Planta(tipoPlanta::atacante, v, d, c, n, a) {}
+    Atacante(int v, int a, int d, int c, int n, char id) : Planta(tipoPlanta::atacante, v, d, c, n, a, id) {}
 };
 
 class Productor : public Planta {
@@ -48,7 +49,7 @@ protected:
     int soles{};
 public:
     Productor() { tipo = tipoPlanta::productor; }
-    Productor(int v, int s, int d, int c, int n) : Planta(tipoPlanta::productor, v, d, c, n), soles(s) {}
+    Productor(int v, int s, int d, int c, int n, char id) : Planta(tipoPlanta::productor, v, d, c, n, id), soles(s) {}
     [[nodiscard]] int habilidadPasiva() const override {
         return soles;
     };
@@ -57,22 +58,22 @@ public:
 class Soporte : public Planta {
 public:
     Soporte() { tipo = tipoPlanta::soporte; };
-    Soporte(int v, int c, int n, int d) : Planta(tipoPlanta::soporte, v, d, c, n) {}
+    Soporte(int v, int c, int n, int d, char id) : Planta(tipoPlanta::soporte, v, d, c, n, id) {}
 };
 
 class LanzaGuisantes : public Atacante {
 public:
-    LanzaGuisantes() : Atacante(300, 20, 3, 100, 1) {}
+    LanzaGuisantes() : Atacante(300, 20, 3, 100, 1, 'L') {}
 };
 
 class LanzaGuisantesDoble : public Atacante {
 public:
-    LanzaGuisantesDoble() : Atacante(300, 40, 3, 150, 1) {}
+    LanzaGuisantesDoble() : Atacante(300, 40, 3, 150, 1, 'D') {}
 };
 
 class LanzaGuisantesHielo : public Atacante {
 public:
-    LanzaGuisantesHielo() : Atacante(300, 20, 3, 175, 1) {}
+    LanzaGuisantesHielo() : Atacante(300, 20, 3, 175, 1, 'H') {}
     void accion(Zombie& z) override {
         z -= ataque;
         !z.congelado ? z.velocidad += 2 : z.velocidad;
@@ -82,12 +83,12 @@ public:
 
 class LanzaGuisantesFuego : public Atacante {
 public:
-    LanzaGuisantesFuego() : Atacante(300, 30, 3, 150, 1) {}
+    LanzaGuisantesFuego() : Atacante(300, 30, 3, 150, 1, 'F') {}
 };
 
 class LanzaMaiz : public Atacante {
 public:
-    LanzaMaiz() : Atacante(300, 20, 3, 100, 1) {}
+    LanzaMaiz() : Atacante(300, 20, 3, 100, 1, 'M') {}
     void accion(Zombie& z) override {
         random_device rd;
         if (rd() % 100 < 30) {
@@ -101,22 +102,22 @@ public:
 
 class Girasol : public Productor {
 public:
-    Girasol() : Productor(300, 50, 49, 50, 1) {}
+    Girasol() : Productor(300, 50, 49, 50, 1, 'G') {}
 };
 
 class GirasolDoble : public Productor {
 public:
-    GirasolDoble() : Productor(300, 100, 49, 50, 1) {}
+    GirasolDoble() : Productor(300, 100, 49, 50, 1, 'G') {}
 };
 
 class Nuez : public Soporte {
 public:
-    Nuez() : Soporte(4000, 50, 1, 0) {}
+    Nuez() : Soporte(4000, 50, 1, 0, 'N') {}
 };
 
 class NuezDoble : public Soporte {
 public:
-    NuezDoble() : Soporte(8000, 125, 1, 0) {}
+    NuezDoble() : Soporte(8000, 125, 1, 0, 'N') {}
 };
 
 inline ostream& operator<<(ostream& os, Planta* planta) {
