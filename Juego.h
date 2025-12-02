9bc732404f4dbd9fd6c7816b1183b38dd7f721f1
rafaelchamayo <166565882+rafaelchamayo@users.noreply.github.com>
2025-12-02 00:35:54 -0500

    Enhance oleadas initialization and logging
    Updated the initialization of 'oleadas' to include additional plant types and fixed the way 'aniadirOleada' is called. Improved the logging of game results by removing redundant code.

--- Juego.h
+++ Juego.h
@@ -25,8 +25,32 @@ private:
 
 public:
     Juego() {
-        oleadas.emplace_back(Oleada({tipoZombie::Comun}, 3, 10, {"Lanzaguisantes"}));
-        oleadas[0].plantas.push_back(new LanzaGuisantes);
+        oleadas.emplace_back(Oleada({tipoZombie::Comun}, 3, 10, {"Lanzaguisantes", "Girasol"}));
+        oleadas[0].plantas.push_back(make_unique<LanzaGuisantes>());
+        oleadas[0].plantas.push_back(make_unique<Girasol>());
+        oleadas.emplace_back(Oleada({tipoZombie::Comun, tipoZombie::Cono}, 3, 15, {"Lanzaguisantes", "Girasol", "Lanzaguisantes Doble"}));
+        oleadas[1].plantas.push_back(make_unique<LanzaGuisantes>());
+        oleadas[1].plantas.push_back(make_unique<Girasol>());
+        oleadas[1].plantas.push_back(make_unique<LanzaGuisantesDoble>());
+        oleadas.emplace_back(Oleada({tipoZombie::Comun, tipoZombie::Cono}, 4, 15, {"Lanzaguisantes", "Girasol", "Lanzaguisantes Doble", "Nuez"}));
+        oleadas[2].plantas.push_back(make_unique<LanzaGuisantes>());
+        oleadas[2].plantas.push_back(make_unique<Girasol>());
+        oleadas[2].plantas.push_back(make_unique<LanzaGuisantesDoble>());
+        oleadas[2].plantas.push_back(make_unique<Nuez>());
+        oleadas.emplace_back(Oleada({tipoZombie::Comun, tipoZombie::Cono, tipoZombie::Atletico }, 4, 20, {"Lanzaguisantes", "Girasol", "Lanzaguisantes Doble", "Nuez", "Lanzaguisantes de Hielo"}));
+        oleadas[3].plantas.push_back(make_unique<LanzaGuisantes>());
+        oleadas[3].plantas.push_back(make_unique<Girasol>());
+        oleadas[3].plantas.push_back(make_unique<LanzaGuisantesDoble>());
+        oleadas[3].plantas.push_back(make_unique<Nuez>());
+        oleadas[3].plantas.push_back(make_unique<LanzaGuisantesHielo>());
+        oleadas.emplace_back(Oleada({tipoZombie::Comun, tipoZombie::Cono, tipoZombie::Atletico, tipoZombie::Cubo }, 4, 30, {"Lanzaguisantes", "Girasol", "Lanzaguisantes Doble", "Nuez", "Lanzaguisantes de Hielo", "Nuez Doble", "Girasol Doble"}));
+        oleadas[4].plantas.push_back(make_unique<LanzaGuisantes>());
+        oleadas[4].plantas.push_back(make_unique<Girasol>());
+        oleadas[4].plantas.push_back(make_unique<LanzaGuisantesDoble>());
+        oleadas[4].plantas.push_back(make_unique<Nuez>());
+        oleadas[4].plantas.push_back(make_unique<LanzaGuisantesHielo>());
+        oleadas[4].plantas.push_back(make_unique<GirasolDoble>());
+
     };
     ~Juego()=default;
 
@@ -34,7 +58,7 @@ public:
         jugador.fasePlantas(oleada);
     };
     bool faseZombies(Oleada &_oleada) {
-        mapa.aniadirOleada(_oleada);
+        mapa.aniadirOleada(&_oleada);
         bool rondaCompleta = mapa.runOleada();
         return rondaCompleta;
     };
@@ -54,16 +78,16 @@ public:
             if (rondaCompleta) {oleadasCompletadas++;}
             if (!rondaCompleta) {cout << jugador.nombre << " perdiste";break;}
         }
+        auto fecha = chrono::system_clock::now();
+        time_t t = chrono::system_clock::to_time_t(fecha);
+        tm *tiempoLocal = localtime(&t);
+        archivo << "Hora: "<<put_time(tiempoLocal, "%Y-%m-%d %H:%M:%S") << endl;
         archivo<<"Nombre: "<<jugador.nombre<<endl;
         archivo<<"Oleadas completadas: "<<oleadasCompletadas<<endl;
         archivo<<"Zombies eliminados: "<<zombiesElimintados<<endl;
         archivo<<"Soles generados: "<<solesGenerados<<endl;
         archivo<<"Dano recibido: "<<dañoRecibido<<endl;
         archivo<<"Puntos finales: "<<zombiesElimintados*30<<endl;
-        auto fecha = chrono::system_clock::now();
-        time_t t = chrono::system_clock::to_time_t(fecha);
-        tm *tiempoLocal = localtime(&t);
-        archivo << put_time(tiempoLocal, "%Y-%m-%d %H:%M:%S") << endl;
         archivo.close();
     }
 
