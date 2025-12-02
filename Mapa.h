ec96c6e9f36b6ffeca33c4a5eda0401b707d025b
rafaelchamayo <166565882+rafaelchamayo@users.noreply.github.com>
2025-12-01 21:19:43 -0500

    Update Mapa.h

--- Mapa.h
+++ Mapa.h
@@ -1,3 +1,7 @@
+//
+// Created by Rafael Chamayo on 29/11/25.
+//
+
 #ifndef MAPA_H
 #define MAPA_H
 
@@ -29,7 +33,7 @@ public:
 
 class Mapa {
 public:
-    vector<vector<Celda>> mapa;
+    vector<vector<Celda>> grilla;
     Oleada oleada{};
     int recursosGenerados{};
     int zombiesUtilizados{};
@@ -38,8 +42,8 @@ public:
     // --------------- CONSTRUCTOR ------------------
 
     Mapa() {
-        mapa.resize(6);
-        for (auto &fila: mapa) {
+        grilla.resize(6);
+        for (auto &fila: grilla) {
             fila.resize(9);
         }
 
@@ -58,18 +62,18 @@ public:
         random_device rd;
         unsigned int numZombie = rd() % oleada.zombies.size();
         tipoZombie zombie = oleada.zombies[numZombie];
-        unsigned int filaRand = rd() % mapa.size();
+        unsigned int filaRand = rd() % grilla.size();
 
         if (zombie == tipoZombie::Comun) {
-            mapa[filaRand][8].zombies.push_back(std::make_unique<ZombieComun>(pair<int, int>(filaRand, 8)));
+            grilla[filaRand][8].zombies.push_back(std::make_unique<ZombieComun>(pair<int, int>(filaRand, 8)));
         } else if (zombie == tipoZombie::Cono) {
-            mapa[filaRand][8].zombies.push_back(std::make_unique<ZombieCono>(pair<int, int>(filaRand, 8)));
+            grilla[filaRand][8].zombies.push_back(std::make_unique<ZombieCono>(pair<int, int>(filaRand, 8)));
         } else if (zombie == tipoZombie::Cubo) {
-            mapa[filaRand][8].zombies.push_back(std::make_unique<ZombieCubo>(pair<int, int>(filaRand, 8)));
+            grilla[filaRand][8].zombies.push_back(std::make_unique<ZombieCubo>(pair<int, int>(filaRand, 8)));
         } else if (zombie == tipoZombie::Atletico) {
-            mapa[filaRand][8].zombies.push_back(std::make_unique<ZombieAtletico>(pair<int, int>(filaRand, 8)));
+            grilla[filaRand][8].zombies.push_back(std::make_unique<ZombieAtletico>(pair<int, int>(filaRand, 8)));
         } else {
-            mapa[filaRand][8].zombies.push_back(std::make_unique<ZombieGigante>(pair<int, int>(filaRand, 8)));
+            grilla[filaRand][8].zombies.push_back(std::make_unique<ZombieGigante>(pair<int, int>(filaRand, 8)));
         }
         zombiesUtilizados++;
     }
@@ -90,7 +94,7 @@ public:
     // ENCONTRAR AL ZOMBIE MAS CERCANO
 
     Zombie* zombieCercano(int fila) {
-        for (auto & j : mapa[fila]) {
+        for (auto & j : grilla[fila]) {
             if (!j.zombies.empty()) {
                 return j.zombies[0].get();
             }
@@ -100,13 +104,29 @@ public:
 
     // AÑADIR PLANTA
 
+    bool aniadirPlanta(string tipoPlanta, int fila, int col) {
+        if (grilla[fila][col].planta != nullptr) {
+            return false;
+        } else {
+            if (tipoPlanta == "Lanzaguisantes") grilla[fila][col].planta = std::make_unique<LanzaGuisantes>();
+            else if (tipoPlanta == "Lanzaguisantes Doble") grilla[fila][col].planta = std::make_unique<LanzaGuisantesDoble>();
+            else if (tipoPlanta == "Lanzaguisantes de Hielo") grilla[fila][col].planta = std::make_unique<LanzaGuisantesHielo>();
+            else if (tipoPlanta == "Lanzaguisantes de fuego") grilla[fila][col].planta = std::make_unique<LanzaGuisantesFuego>();
+            else if (tipoPlanta == "Lanzamaiz") grilla[fila][col].planta = std::make_unique<LanzaMaiz>();
+            else if (tipoPlanta == "Girasol") grilla[fila][col].planta = std::make_unique<Girasol>();
+            else if (tipoPlanta == "Girasol Doble") grilla[fila][col].planta = std::make_unique<GirasolDoble>();
+            else if (tipoPlanta == "Nuez") grilla[fila][col].planta = std::make_unique<Nuez>();
+            else if (tipoPlanta == "Nuez Doble") grilla[fila][col].planta = std::make_unique<NuezDoble>();
+            return true;
+        }
 
+    }
 
     // VERIFICAR SI YA SE USARON TODOS LOS ZOMBIES Y YA NO HAY EN EL MAPA
 
     bool oleadaIncompleta() {
         if (zombiesUtilizados == oleada.cantZombies) {
-            for (auto & i : mapa) {
+            for (auto & i : grilla) {
                 for (auto & j : i) {
                     if (!j.zombies.empty()) { return true; }
                 }
@@ -118,8 +138,22 @@ public:
 
     //IMPRIMIR UNA CELDA
 
-    void imprimirCelda() {
-        
+    void imprimirCelda(const Celda& celda) {
+        string contenido{};
+        if (celda.planta != nullptr) {
+            contenido+= 'P';
+        }
+        int contzombies{};
+        if (!celda.zombies.empty()) {
+            for (int i = 0; i < celda.zombies.size(); i++) {
+                contzombies++;
+            }
+            contenido += " " + to_string(contzombies) + "Z";
+        }
+        if (contenido.empty()) {
+            contenido += '-';
+        }
+        cout << setw(10) << left << contenido;
     }
 
     // ------------------ ENGINE ------------------
@@ -128,7 +162,7 @@ public:
 
         // ACTIVAR HABILIDADES PASIVAS
 
-        for (auto & i : mapa) {
+        for (auto & i : grilla) {
             for (auto & j : i) {
                 if (j.planta != nullptr) {
                     if (j.planta->tipo == tipoPlanta::productor) {
@@ -142,14 +176,14 @@ public:
 
         // ACTIVAS HABILIDADES PLANTAS ACTIVAS
 
-        for (int i = 0; i < mapa.size(); i++) {
-            for (int j = 0; j < mapa[i].size(); j++) {
-                if (mapa[i][j].planta != nullptr) {
-                    if (mapa[i][j].planta->tipo == tipoPlanta::atacante) {
-                        if (mapa[i][j].planta->anadirTick()) {
+        for (int i = 0; i < grilla.size(); i++) {
+            for (int j = 0; j < grilla[i].size(); j++) {
+                if (grilla[i][j].planta != nullptr) {
+                    if (grilla[i][j].planta->tipo == tipoPlanta::atacante) {
+                        if (grilla[i][j].planta->anadirTick()) {
                             Zombie* dum = zombieCercano(i);
                             if (dum != nullptr) {
-                                mapa[i][j].planta->accion(*dum);
+                                grilla[i][j].planta->accion(*dum);
                             }
                         }
                     }
@@ -160,17 +194,17 @@ public:
 
         // MOVIMIENTO ZOMBIES
 
-        for (int i = 1; i < mapa.size(); i++) {
-            for (int j = 0; j < mapa[i].size(); j++) {
-                if (!mapa[i][j].zombies.empty()) {
-                    for (int z = 0; z < mapa[i][j].zombies.size();) {
-                        if (mapa[i][j].zombies[z]->anadirTick()) {
-                            if (mapa[i][j].planta != nullptr) {mapa[i][j].zombies[z]->plantaCerca = true;}
-                            mapa[i][j].zombies[z]->actuar();
-                            int newcol = mapa[i][j].zombies[z]->getPos().second;
+        for (int i = 1; i < grilla.size(); i++) {
+            for (int j = 0; j < grilla[i].size(); j++) {
+                if (!grilla[i][j].zombies.empty()) {
+                    for (int z = 0; z < grilla[i][j].zombies.size();) {
+                        if (grilla[i][j].zombies[z]->anadirTick()) {
+                            if (grilla[i][j].planta != nullptr) {grilla[i][j].zombies[z]->plantaCerca = true;}
+                            grilla[i][j].zombies[z]->actuar();
+                            int newcol = grilla[i][j].zombies[z]->getPos().second;
                             if (newcol != j) {
-                                mapa[i][newcol].zombies.push_back(std::move(mapa[i][j].zombies[z]));
-                                mapa[i][j].zombies.erase(mapa[i][j].zombies.begin() + z);
+                                grilla[i][newcol].zombies.push_back(std::move(grilla[i][j].zombies[z]));
+                                grilla[i][j].zombies.erase(grilla[i][j].zombies.begin() + z);
                             } else {
                                 z++;
                             }
@@ -184,27 +218,27 @@ public:
 
         // COLISION Y VIDA PLANTAS Y ZOMBIES
 
-        for (int i = 0; i < mapa.size(); i++) {
-            for (int j = 0; j < mapa[i].size(); j++) {
-                if (!mapa[i][j].zombies.empty()) { // Limpieza de zombies
-                    for (int z = 0; z < mapa[i][j].zombies.size();) {
-                        if (mapa[i][j].zombies[z]->getVida()== 0) {
-                            mapa[i][j].zombies.erase(mapa[i][j].zombies.begin() + z);
+        for (int i = 0; i < grilla.size(); i++) {
+            for (int j = 0; j < grilla[i].size(); j++) {
+                if (!grilla[i][j].zombies.empty()) { // Limpieza de zombies
+                    for (int z = 0; z < grilla[i][j].zombies.size();) {
+                        if (grilla[i][j].zombies[z]->getVida()== 0) {
+                            grilla[i][j].zombies.erase(grilla[i][j].zombies.begin() + z);
                         } else {
                             z++;
                         }
                     }
                 }
 
-                if (mapa[i][j].planta != nullptr) {
-                    if (!mapa[i][j].zombies.empty()) {
+                if (grilla[i][j].planta != nullptr) {
+                    if (!grilla[i][j].zombies.empty()) {
                         int dmg{};
-                        for (int z = 0; z < mapa[i][j].zombies.size();z++) {
-                            dmg += mapa[i][j].zombies[z]->danio;
+                        for (int z = 0; z < grilla[i][j].zombies.size();z++) {
+                            dmg += grilla[i][j].zombies[z]->danio;
                         }
-                        *mapa[i][j].planta -= dmg;
-                        if (mapa[i][j].planta->getVida() == 0) {
-                            mapa[i][j].planta.reset();
+                        *grilla[i][j].planta -= dmg;
+                        if (grilla[i][j].planta->getVida() == 0) {
+                            grilla[i][j].planta.reset();
                         }
                     }
                 }
@@ -213,7 +247,7 @@ public:
 
         // CHEQUEAR SI SE JUGADOR PERDIO
         bool perdioJuego = false;
-        for (auto & i : mapa) {
+        for (auto & i : grilla) {
             if (!i[0].zombies.empty()) {
                 perdioJuego = true;
                 break;
@@ -223,7 +257,13 @@ public:
     }
 
     void imprimir() {
-
+        for (int i = 0; i < grilla.size(); i++) {
+            for (int j = 0; j < grilla[i].size(); j++) {
+                imprimirCelda(grilla[i][j]);
+            }
+            cout << endl;
+        }
+        cout << endl << endl;
     }
 
     // ------------------ MAIN ENGINE ------------------
@@ -231,7 +271,7 @@ public:
     bool runOleada() {
         bool completaOleada = true;
         while (oleadaIncompleta()) {
-            this_thread::sleep_for(chrono::milliseconds(500));
+            this_thread::sleep_for(chrono::milliseconds(1000));
             tickZombie();
 
             bool perdioJuego = this->runTurno();
@@ -239,6 +279,7 @@ public:
                 completaOleada = false;
                 break;
             }
+            imprimir();
         }
         return completaOleada;
     }
